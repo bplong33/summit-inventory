96046be496905b4bf6a9771f24d6ba6acd8a537f
bplong33 <bplong96@gmail.com>
2022-07-05 13:48:02 -0400

    feat((inventory model)): Added soft_delete and restore functions to Inventory model.

--- summitinventory/inventory/models.py
+++ summitinventory/inventory/models.py
@@ -6,9 +6,13 @@ class Inventory(models.Model):
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=500)
     quantity = models.IntegerField()
+    is_deleted = models.BooleanField(default=False)
     
     def __str__(self):
         return self.name
     
-    # def set(self, field, value):
-        
+    def soft_delete(self):
+        self.is_deleted = True
+    
+    def restore(self):
+        self.is_deleted = False
