311cd95d9a6b118b13b7f38860c1eedbe9dc21ad
bplong33 <bplong96@gmail.com>
2022-07-06 12:12:38 -0400

    Changed max_length on password field.

--- summitinventory/authtoken/models.py
+++ summitinventory/authtoken/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class InventoryUser(AbstractUser):
     username = models.CharField(max_length=25, unique=True)
-    password = models.CharField(max_length=20)
+    password = models.CharField(max_length=100)
     permissions = models.IntegerField(default=0)
     token = models.CharField(max_length=1000, default=0)
     
